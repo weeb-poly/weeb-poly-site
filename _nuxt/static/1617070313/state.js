cf7dff11c17aadf0f645f458bd46e913db55edcb
tcyrus <tcyrus@users.noreply.github.com>
2021-03-30 02:11:58 +0000

    deploy: 3c863cf98db40bcb9a315a27ea8a5a2245c0c297

---
+++ _nuxt/static/1617070313/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617070313",layout:"default",error:a,state:{quotes:{list:[]}},serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bc927735"}}}}(null,"\u002F"));
