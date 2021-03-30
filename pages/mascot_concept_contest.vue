1602004a8df9703037ab151819f4eb0b86f86556
Timothy Cyrus <tim.h.cyrus@gmail.com>
2021-03-29 22:22:19 -0400

    Mascot Concept Contest Page

---
+++ pages/mascot_concept_contest.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <main id="main">
+      <div class="breadcrumbs">
+        <div class="container">
+          <h2>Mascot Concept Competiton</h2>
+          <p>
+            These are the mascots concepts from our mascot concept competition.
+            Rankings were decided by votes on a public poll which ran from 3/21/2021 18:00 EDT to 3/28/2021 22:00 EDT.
+            The current version of the mascot may differ from the submitted version.
+            We've shortened mascot descriptions to fit on the page.
+            All server mascots will be recieving their own wiki-like page with more information (WIP).
+          </p>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row my-1 row-cols-1 row-cols-md-3 g-4 justify-content-md-center">
+
+          <div class="col">
+            <div class="card mb-3">
+              <h5 class="card-header">1<sup>st</sup> Place - 18 Votes</h5>
+              <img class="card-img-top" src="../assets/img/contest/shion.png">
+              <div class="card-body">
+                <h5 class="card-title">Shion</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Cy (Bells)</h6>
+                <p class="card-text">
+                  A grad student at WPI.
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">Primary Mascot</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card mb-3">
+              <h5 class="card-header">2<sup>nd</sup> Place - 13 Votes</h5>
+              <img class="card-img-top" src="../assets/img/contest/akari.png">
+              <div class="card-body">
+                <h5 class="card-title">Akari</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Bells</h6>
+                <p class="card-text">
+                  <i>Unknown</i>
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">Alternate Mascot</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card mb-3">
+              <h5 class="card-header">3<sup>rd</sup> Place - 12 Votes</h5>
+              <img class="card-img-top" src="../assets/img/contest/fukaya.png">
+              <div class="card-body">
+                <h5 class="card-title">Fukaya</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Bowserinator</h6>
+                <p class="card-text">
+                  An RPI CS Major who moved from Japan.
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">Academic Mascot</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card mb-3">
+              <h5 class="card-header">Honorable Mention</h5>
+              <img class="card-img-top" src="../assets/img/contest/wolfgirl.png">
+              <div class="card-body">
+                <h5 class="card-title">Wolfgirl</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Chorogon</h6>
+                <p class="card-text">
+                  <i>Unknown</i>
+                </p>
+              </div>
+            </div>
+          </div>
+          
+          <div class="col">
+            <div class="card mb-3">
+              <h5 class="card-header">Honorable Mention</h5>
+              <img class="card-img-top" src="../assets/img/contest/mai.png">
+              <div class="card-body">
+                <h5 class="card-title">Mai</h5>
+                <h6 class="card-subtitle mb-2 text-muted">ShapeShifter</h6>
+                <p class="card-text">
+                  A mischevious elementary school student who managed to sneak into WPI.
+                </p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
