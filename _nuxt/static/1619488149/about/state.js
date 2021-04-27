10d205b23dbf76e02dfcd9cedd036c1274bc81c8
tcyrus <tcyrus@users.noreply.github.com>
2021-04-27 01:49:10 +0000

    deploy: 6f481981d5d81295fe70fe39fee3cdb6c75fa51c

---
+++ _nuxt/static/1619488149/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619488149",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{quotes:{list:[]}},serverRendered:false,routePath:a,config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"05cdce93"}}}}("\u002Fabout"));
