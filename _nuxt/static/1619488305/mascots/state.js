e0efc0c7fcdbcc4c18ea1ed837c260d735061091
tcyrus <tcyrus@users.noreply.github.com>
2021-05-01 23:25:26 +0000

    deploy: 332c5ddbffa21b2ebbeaf7f506fbf0ff88a6d3f8

--- _nuxt/static/1619488305/mascots/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1619488305",layout:"default",error:a,state:{quotes:{list:[]}},serverRendered:true,routePath:"\u002Fmascots",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"85074557"}}}}(null));
