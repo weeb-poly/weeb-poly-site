3c8e3beee4a797365743aa334854485d0224e9ee
Timothy Cyrus <tim.h.cyrus@gmail.com>
2021-05-09 15:20:40 -0400

    Added Think Meme
    Art has been added with permission of the original creator

--- components/About.vue
+++ components/About.vue
@@ -3,22 +3,32 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-6 order-1 order-lg-2">
-          <img src="https://via.placeholder.com/1024x768.jpg" class="img-fluid" alt="">
+          <img src="~/assets/img/think.png" class="img-fluid" alt="Think, Shirley! Think!">
         </div>
         <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
-          <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-          <p class="font-italic">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua.
+          <h3>Think, Shirley! Think!</h3>
+          <p>
+            You have one of the top Cybersecurity Teams, but you still haven't figured out how to isolate your network.
           </p>
+          <p>
+            You keep on raising tuition, but you keep cutting programs.
+          </p>
+          <p>
+            Your reign will outlast every insignificant little being on this campus.
+          </p>
+          <p>
+            You'll live to see this campus crumble to dust and blow away.
+          </p>
+          <p>
+            <b>What will you have after 500 years?</b>
+          </p>
+          <!--
           <ul>
             <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
             <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
             <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
           </ul>
-          <p>
-            Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-          </p>
+          -->
         </div>
       </div>
     </div>
